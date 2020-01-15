d0c7eaed4c0dd02317a471a15954816700f64e09
agusmagne <agusmagne@gmail.com>
2020-01-15 16:50:14 -0300

    Done

--- springboot-servicio-farm/src/main/java/com/formacionbdi/springboot/app/farm/repository/FarmRepository.java
+++
@@ -1,9 +0,0 @@
-package com.formacionbdi.springboot.app.farm.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.formacionbdi.springboot.app.farm.models.entity.Farm;
-
-public interface FarmRepository extends JpaRepository<Farm, Long>{
-
-}
